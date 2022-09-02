37107bbc8c4235d2296139f1a1c311e9f35136dc
AntaNea <105243666+AntaNea@users.noreply.github.com>
2022-09-02 16:09:38 +0300

    Add files via upload

---
+++ two/Second/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Google.Protobuf;
+using ProtoBuf;
+
+namespace Second
+{
+    class Program
+    {
+        static void ShowAsText(People p) => Console.WriteLine("{p.Id}: Hello, My name is {p.Name}, I'm {p.Age}");
+
+    static void Main()
+    {
+        People p0 = new People { Id = 0, Name = "Adam", Age = 18, IsMan = true };//Experemental p
+        ShowAsText(p0);
+        
+        var p1 = p0.ToByteArray(); //Convert to array
+        
+        foreach (byte p in p1) //how it sees
+            Console.Write(" {p}");
+        
+        People p2 = new People(); //Back convert to People
+        
+        ShowAsText(p2);
+
+       
+    }
+  
+}
+}
